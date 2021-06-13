8a41a2e35fc7195a862101b095a516c80a68793b
Davide Berardi <davidebrr@gmail.com>
2021-06-13 18:05:15 +0200

    add styled-components lib, add resources data

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Typeahead from './Modules/Typeahead';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Typeahead />
   </React.StrictMode>,
   document.getElementById('root')
 );
